e09e64f8b1f8c35d3380c4e3a3e9c3203136af3a
LIZ ESTEBAM DE OLIVEIRA <0030482321013@lti.fatecso.edu.br>
2025-03-24 12:18:14 -0300

    exercicios do dia 24/03

---
+++ exerciciosJs/media.js
@@ -0,0 +1,7 @@
+let nome = prompt("Informe o nome do aluno(a)");
+let nota1 = parseFloat(prompt("Informe a 1 nota"));
+let nota2 = parseFloat(prompt("Informe a 2 nota"));
+let nota3 = parseFloat(prompt("Informe a 3 nota"));
+let nota4 = parseFloat(prompt("Informe a 4 nota"));
+
+alert("A média do aluno(a) " + nome + " é " + (nota1+nota2+nota3+nota4)/4);
---
+++ exerciciosJs/operacoes.js
@@ -0,0 +1,8 @@
+num1 = parseFloat(prompt("Informe o 1º numero:"));
+num2 = parseFloat(prompt("Informe o 2º numero:"));
+
+alert("A soma dos 2 números: " + (num1 + num2)+
+        "\nA subtração do 1º pelo 2º: " + (num1 - num2)+
+        "\nO produto dos números: " + (num1 * num2)+
+        "\nA divisão do 1º pelo 2º: " + (num1/num2)+
+        "\nO resto da divisão do 1º pelo 2º: " + (num1%num2));
